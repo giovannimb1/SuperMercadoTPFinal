5a06169da27f0591e106fd917411c4811065a222
Giovanni <morrobaigiovannisc@gmail.com>
2025-11-17 16:39:30 -0300

    manejo el precio del carrito segun se añadan o eliminen productos del mismo q me habia olvidado

--- src/Codigo/Logica/Carrito.java
+++ src/Codigo/Logica/Carrito.java
@@ -9,7 +9,6 @@ public class Carrito {
     /// ATRIBUTOS
     private List<Producto> productos = new ArrayList<>();
     private double total = 0;
-    private boolean finalizado = false;
     private Map<Integer, Producto> archivo = Almacen.getInstancia().getProductos();
 
     /// CONSTRUCTOR
@@ -23,18 +22,24 @@ public class Carrito {
         return productos;
     }
 
+    public double getTotal() {
+        return total;
+    }
+
     /// METODOS
 
     public boolean agregarProducto(int id){
         boolean flag = false;
         if(archivo.get(id)!=null && archivo.get(id).reducirStock()){
             flag = productos.add(archivo.get(id));
+            total+=productos.get(id).getPrecio();
         }
      return flag;
     }
 
     public boolean eliminarProducto(Producto p){
         p.aumentarStock();
+        total-=p.getPrecio();
         return productos.remove(p);
     }
 
